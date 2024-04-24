423b3ecb7ddf24f340461621d1166fbd2f76ba67
chano64123 <promo201674@gmail.com>
2024-04-23 23:31:39 -0500

    events js

--- store-dom/script/products.js
+++ store-dom/script/products.js
@@ -10,10 +10,10 @@ class Product {
 }
 
 const product1 = new Product('assets/mock1.jpg', 'Macbook Pro 15\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
-const product2 = new Product('assets/mock1.jpg', 'Macbook Pro 15\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
-const product3 = new Product('assets/mock1.jpg', 'Macbook Pro 15\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
-const product4 = new Product('assets/mock1.jpg', 'Macbook Pro 15\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
-const product5 = new Product('assets/mock1.jpg', 'Macbook Pro 15\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
-const product6 = new Product('assets/mock1.jpg', 'Macbook Pro 15\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
+const product2 = new Product('assets/mock1.jpg', 'Macbook Pro 14\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
+const product3 = new Product('assets/mock1.jpg', 'Macbook Pro 13\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
+const product4 = new Product('assets/mock1.jpg', 'Macbook Pro 12\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
+const product5 = new Product('assets/mock1.jpg', 'Macbook Pro 11\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
+const product6 = new Product('assets/mock1.jpg', 'Macbook Pro 10\'4', 'Space Gray', 2500, 60, 'Incluye impuesto País y percepción AFIP');
 
 const products = [product1, product2, product3, product4, product5, product6];
