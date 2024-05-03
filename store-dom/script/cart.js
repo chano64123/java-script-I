a109760e4fc25288085b5b460b7fc8a570feb1ec
chano64123 <promo201674@gmail.com>
2024-05-02 20:49:48 -0500

    eventos III

--- store-dom/script/cart.js
+++ store-dom/script/cart.js
@@ -1,21 +1,46 @@
 const cartProductsString = localStorage.getItem('cart');
-const cartProducts = JSON.parse(cartProductsString);
+let cartProducts = JSON.parse(cartProductsString) ?? [];
 
 const cartProductsSelector = document.getElementById('cart-products');
 const totalPriceSelector = document.getElementById('total-price');
 
-const printCartProducts = (products) => {
-  //!generando los elementos del carrito
-  let templateProducts = '';
-  for (let product of products) {
-    templateProducts += getCartProductTemplate(product);
-  }
-  cartProductsSelector.innerHTML = templateProducts;
-  //!obteniendo el total a pagar
+const setTotal = (products) => {
   let totalPrice = 0;
   products.forEach(product => (totalPrice += product.price * product.quantity));
-  console.log(totalPrice);
   totalPriceSelector.textContent = totalPrice.toFixed(2);
+}
+
+const printCartProducts = (products) => {
+  let templateProducts = '';
+  if (products.length > 0) {
+    //!generando los elementos del carrito
+    for (let product of products) {
+      templateProducts += getCartProductTemplate(product);
+    }
+    //!obteniendo el total a pagar
+    setTotal(products);
+  } else {
+    templateProducts = getMessageNoProducts();
+  }
+  cartProductsSelector.innerHTML = templateProducts;
+}
+
+const changeQuantity = (event) => {
+  const newQuantity = Number(event.target.value);
+  const idProduct = Number(event.target.id);
+  const colorProduct = event.target.dataset.color;
+
+  const product = cartProducts.find(product => product.id === idProduct && product.color === colorProduct);
+  product.quantity = newQuantity;
+
+  localStorage.setItem('cart', JSON.stringify(cartProducts));
+  setTotal(cartProducts);
+}
+
+const getMessageNoProducts = () => {
+  return `
+    <h1 class="no-products-message">No hay productos en el carrito.</h1>
+  `;
 }
 
 const getCartProductTemplate = (product) => {
@@ -26,13 +51,11 @@ const getCartProductTemplate = (product) => {
           <span class="cart-product-title">${product.title}</span>
           <span class="cart-product-color">${product.color}</span>
           <span class="cart-product-description">${product.description}</span>
-          <input type="number" value="${product.quantity}" class="cart-product-quantity" />
+          <input id="${product.id}" data-color="${product.color}" type="number" min="1" value="${product.quantity}" class="cart-product-quantity" onchange="changeQuantity(event)" />
       </div>
       <div class="price">S/${product.price.toFixed(2)}</div>
     </div>
   `;
 }
 
-if (cartProducts) {
-  printCartProducts(cartProducts);
-}  
+printCartProducts(cartProducts);
